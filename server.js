fad406374d005f6a09ee47bc4d3dcf7136d0c040
nicoleluguesi@gmail.com <nicoleluguesi.student@careerdevs.com>
2020-05-28 20:51:05 -0400

    fix git ignore

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+//const connectDB = require('./config/db');
+const { mongoUri } = require('./config')
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true  } )
+.then(() => console.log("Connected to db"))
+.catch((error) => console.error("Failed to connect to db", error));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server has started on port ${port}`));
+
+
+// cors accepts request from any system - security of site
+
